837f56925df3e2ab13424db9deb1ce0b9a457afd
fatememokhtari.84@gmail.com <fatememokhtari.84@gmail.com>
2021-04-12 21:32:47 +0430

    View Component & Partial View

---
+++ NeGa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NeGa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
